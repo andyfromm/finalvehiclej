678d5876641c1441a1ca66a37742fc78723da323
mmcfad <mmcfad@udel.edu>
2014-12-07 15:27:18 -0500

    commit

---
+++ JUnitFinal/src/JUnitPackage/JUnitVehicleTest.java
@@ -0,0 +1,38 @@
+package JUnitPackage;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import VehiclePackage.Car;
+
+public class JUnitVehicleTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	assertNotEquals(c.NumberOfDoors,t.NumberOfAxels);
+
+}
